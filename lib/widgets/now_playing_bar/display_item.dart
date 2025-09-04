2e34a46314630824afb2de099e83e8e2579ec3c1
Hoang Btmrk <hoang@bitmark.com>
2025-09-04 14:08:31 +0700

    fix tap area

--- lib/widgets/now_playing_bar/display_item.dart
+++ lib/widgets/now_playing_bar/display_item.dart
@@ -31,138 +31,142 @@ class DisplayItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Opacity(
-        opacity: isPlaying ? 1 : 0.5,
-        child: Row(
-          crossAxisAlignment: isInExpandedView
-              ? CrossAxisAlignment.center
-              : CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: DisplayItemTokens.thumbWidth,
-              height: DisplayItemTokens.thumbHeight.toDouble(),
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 0,
-                    top: 0,
-                    child: Container(
-                      width: DisplayItemTokens.thumbImageWidth,
-                      height: DisplayItemTokens.thumbImageHeight.toDouble(),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(0),
-                      ),
-                      child: tokenGalleryThumbnailWidget(
-                        context,
-                        CompactedAssetToken.fromAssetToken(assetToken),
-                        DisplayItemTokens.thumbImageWidth.toInt(),
-                        ratio: DisplayItemTokens.thumbImageWidth /
-                            DisplayItemTokens.thumbImageHeight,
-                        useHero: false,
-                      ),
-                    ),
-                  ),
-                  // Positioned(
-                  //   left: (DisplayItemTokens.thumbWidth -
-                  //           DisplayItemTokens.thumbProgressBarWidth) /
-                  //       2,
-                  //   bottom: (DisplayItemTokens.thumbWidth -
-                  //           DisplayItemTokens.thumbProgressBarWidth) /
-                  //       2,
-                  //   child: Container(
-                  //     width: DisplayItemTokens.thumbProgressBarWidth,
-                  //     padding: EdgeInsets.symmetric(
-                  //       horizontal: DisplayItemTokens
-                  //           .thumbProgressBarPaddingHorizontal
-                  //           .toDouble(),
-                  //       vertical: DisplayItemTokens.thumbProgressBarPaddingBottom
-                  //           .toDouble(),
-                  //     ),
-                  //     decoration: BoxDecoration(
-                  //       color: DisplayItemTokens.thumbProgressBarBgColor,
-                  //       borderRadius: BorderRadius.circular(
-                  //         DisplayItemTokens.thumbProgressBarCornerRadius.toDouble(),
-                  //       ),
-                  //     ),
-                  //     child: LinearProgressIndicator(
-                  //       minHeight: DisplayItemTokens.thumbProgressBarStrokeWeight
-                  //           .toDouble(),
-                  //       value: 0.5,
-                  //       color: DisplayItemTokens.thumbProgressBarProgressColor,
-                  //       backgroundColor:
-                  //           DisplayItemTokens.thumbProgressBarBarBgColor,
-                  //       borderRadius: BorderRadius.circular(
-                  //         DisplayItemTokens.thumbProgressBarCornerRadius.toDouble(),
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
-                ],
-              ),
-            ),
-            SizedBox(width: DisplayItemTokens.gap.toDouble()),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  if (deviceName != null)
-                    Text(
-                      deviceName!.toUpperCase(),
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                      style: TextStyle(
-                        fontSize: DisplayItemTokens.textDeviceFontSize.toDouble(),
-                        fontWeight: FontWeightUtil.fromString(
-                          DisplayItemTokens.textDeviceFontWeight,
+      child: Container(
+        color: Colors.transparent,
+        width: double.infinity,
+        child: Opacity(
+          opacity: isPlaying ? 1 : 0.5,
+          child: Row(
+            crossAxisAlignment: isInExpandedView
+                ? CrossAxisAlignment.center
+                : CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: DisplayItemTokens.thumbWidth,
+                height: DisplayItemTokens.thumbHeight.toDouble(),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 0,
+                      top: 0,
+                      child: Container(
+                        width: DisplayItemTokens.thumbImageWidth,
+                        height: DisplayItemTokens.thumbImageHeight.toDouble(),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(0),
                         ),
-                        height: DisplayItemTokens.textDeviceLineHeight /
-                            DisplayItemTokens.textDeviceFontSize,
-                        color: DisplayItemTokens.textColor,
+                        child: tokenGalleryThumbnailWidget(
+                          context,
+                          CompactedAssetToken.fromAssetToken(assetToken),
+                          DisplayItemTokens.thumbImageWidth.toInt(),
+                          ratio: DisplayItemTokens.thumbImageWidth /
+                              DisplayItemTokens.thumbImageHeight,
+                          useHero: false,
+                        ),
                       ),
                     ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      BlocBuilder<IdentityBloc, IdentityState>(
-                        bloc: _identityBloc,
-                        builder: (context, state) {
-                          final artistTitle = assetToken.artistName
-                                  ?.toIdentityOrMask(state.identityMap) ??
-                              assetToken.artistName;
-                          return Text(
-                            artistTitle ?? '',
+                    // Positioned(
+                    //   left: (DisplayItemTokens.thumbWidth -
+                    //           DisplayItemTokens.thumbProgressBarWidth) /
+                    //       2,
+                    //   bottom: (DisplayItemTokens.thumbWidth -
+                    //           DisplayItemTokens.thumbProgressBarWidth) /
+                    //       2,
+                    //   child: Container(
+                    //     width: DisplayItemTokens.thumbProgressBarWidth,
+                    //     padding: EdgeInsets.symmetric(
+                    //       horizontal: DisplayItemTokens
+                    //           .thumbProgressBarPaddingHorizontal
+                    //           .toDouble(),
+                    //       vertical: DisplayItemTokens.thumbProgressBarPaddingBottom
+                    //           .toDouble(),
+                    //     ),
+                    //     decoration: BoxDecoration(
+                    //       color: DisplayItemTokens.thumbProgressBarBgColor,
+                    //       borderRadius: BorderRadius.circular(
+                    //         DisplayItemTokens.thumbProgressBarCornerRadius.toDouble(),
+                    //       ),
+                    //     ),
+                    //     child: LinearProgressIndicator(
+                    //       minHeight: DisplayItemTokens.thumbProgressBarStrokeWeight
+                    //           .toDouble(),
+                    //       value: 0.5,
+                    //       color: DisplayItemTokens.thumbProgressBarProgressColor,
+                    //       backgroundColor:
+                    //           DisplayItemTokens.thumbProgressBarBarBgColor,
+                    //       borderRadius: BorderRadius.circular(
+                    //         DisplayItemTokens.thumbProgressBarCornerRadius.toDouble(),
+                    //       ),
+                    //     ),
+                    //   ),
+                    // ),
+                  ],
+                ),
+              ),
+              SizedBox(width: DisplayItemTokens.gap.toDouble()),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (deviceName != null)
+                      Text(
+                        deviceName!.toUpperCase(),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontSize: DisplayItemTokens.textDeviceFontSize.toDouble(),
+                          fontWeight: FontWeightUtil.fromString(
+                            DisplayItemTokens.textDeviceFontWeight,
+                          ),
+                          height: DisplayItemTokens.textDeviceLineHeight /
+                              DisplayItemTokens.textDeviceFontSize,
+                          color: DisplayItemTokens.textColor,
+                        ),
+                      ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        BlocBuilder<IdentityBloc, IdentityState>(
+                          bloc: _identityBloc,
+                          builder: (context, state) {
+                            final artistTitle = assetToken.artistName
+                                    ?.toIdentityOrMask(state.identityMap) ??
+                                assetToken.artistName;
+                            return Text(
+                              artistTitle ?? '',
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                              style: Theme.of(context).textTheme.small,
+                            );
+                          },
+                        ),
+                        Transform.translate(
+                          offset: Offset(
+                            0,
+                            DisplayItemTokens.textArtworkGap.toDouble(),
+                          ),
+                          child: Text(
+                            assetToken.title ?? '',
                             overflow: TextOverflow.ellipsis,
                             maxLines: 1,
-                            style: Theme.of(context).textTheme.small,
-                          );
-                        },
-                      ),
-                      Transform.translate(
-                        offset: Offset(
-                          0,
-                          DisplayItemTokens.textArtworkGap.toDouble(),
+                            style: isInExpandedView
+                                ? Theme.of(context).textTheme.small.copyWith(
+                                      fontWeight: FontWeightUtil.fromString(
+                                        PrimitivesTokens.fontWeightsBold,
+                                      ),
+                                      fontStyle: FontStyle.italic,
+                                    )
+                                : Theme.of(context).textTheme.small,
+                          ),
                         ),
-                        child: Text(
-                          assetToken.title ?? '',
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                          style: isInExpandedView
-                              ? Theme.of(context).textTheme.small.copyWith(
-                                    fontWeight: FontWeightUtil.fromString(
-                                      PrimitivesTokens.fontWeightsBold,
-                                    ),
-                                    fontStyle: FontStyle.italic,
-                                  )
-                              : Theme.of(context).textTheme.small,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
