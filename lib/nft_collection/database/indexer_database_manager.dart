06b87318dcb09bc22e9b42ed1d4c57990c10023b
PuPha <nguyensanghso@gmail.com>
2025-10-13 15:41:35 +0700

    Check null

--- lib/nft_collection/database/indexer_database_manager.dart
+++ lib/nft_collection/database/indexer_database_manager.dart
@@ -78,7 +78,9 @@ class IndexerDataBaseObjectBox implements IndexerDatabaseAbstract {
     tokenObject.asset.target = assetObject;
     try {
       // we have to put the asset and provenance objects
-      assetBox.put(tokenObject.asset.target!);
+      if (tokenObject.asset.target != null) {
+        assetBox.put(tokenObject.asset.target!);
+      }
       provenanceBox.putMany(tokenObject.provenance);
       final tokenId = assetTokenBox.put(tokenObject);
       return tokenId;
