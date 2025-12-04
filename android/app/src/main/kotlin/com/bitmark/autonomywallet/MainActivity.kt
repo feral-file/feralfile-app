d56da54f7c679f8591ff504488a585c1c0f89eff
PuPha <nguyensanghso@gmail.com>
2025-12-04 16:37:12 +0700

    Revert "remove kotlin/com/bitmark/autonomywallet"
    This reverts commit ea0d4b15c45c1af12b5624f99af0595c0ebb4746.

---
+++ android/app/src/main/kotlin/com/bitmark/autonomywallet/MainActivity.kt
@@ -0,0 +1,63 @@
+/* SPDX-License-Identifier: BSD-2-Clause-Patent
+ * Copyright © 2022 Bitmark. All rights reserved.
+ * Use of this source code is governed by the BSD-2-Clause Plus Patent License
+ * that can be found in the LICENSE file.
+ */
+
+package com.feralfile.app
+
+import android.os.Build
+import android.os.Bundle
+import android.content.Intent
+import androidx.activity.enableEdgeToEdge
+import com.feralfile.app.FileLogger
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+
+class MainActivity : FlutterFragmentActivity() {
+
+    private val CHANNEL = "com.feralfile.wallet/log"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        FileLogger.init(applicationContext)
+        // verity signing certificate
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            window.setHideOverlayWindows(true)
+        }
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "getLogContent" -> {
+                    try {
+                        val logContent = FileLogger.getLogContent()
+                        result.success(logContent)
+                    } catch (e: Exception) {
+                        result.error("UNAVAILABLE", "Could not get log content", e.toString())
+                    }
+                }
+
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+    }
+}
