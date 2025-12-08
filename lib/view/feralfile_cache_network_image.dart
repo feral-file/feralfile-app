0533235cf1ee18b74b52951867ca43aca4154602
Hoang Nguyen <hoang@bitmark.com>
2025-12-08 15:57:58 +0700

    Add logs for Image load error

--- lib/view/feralfile_cache_network_image.dart
+++ lib/view/feralfile_cache_network_image.dart
@@ -1,4 +1,5 @@
 import 'package:autonomy_flutter/common/injector.dart';
+import 'package:autonomy_flutter/util/log.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cached_network_image_platform_interface/cached_network_image_platform_interface.dart';
 import 'package:flutter/cupertino.dart';
@@ -147,6 +148,8 @@ class FFCacheNetworkImage extends CachedNetworkImage {
     Object error,
     StackTrace? stackTrace,
   ) {
+    log.info(
+        'FFCacheNetworkImageError: url: $imageUrl, error: $error, stackTrace: $stackTrace');
     return errorWidget!(context, imageUrl, error);
   }
 }
