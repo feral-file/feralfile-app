257a1d45cd9bba8436e796ebcaa25c62cc3b1ace
PuPha <nguyensanghso@gmail.com>
2025-12-15 09:33:17 +0700

    Update

--- lib/screen/device_setting/now_displaying_page.dart
+++ lib/screen/device_setting/now_displaying_page.dart
@@ -21,6 +21,7 @@ import 'package:autonomy_flutter/util/style.dart';
 import 'package:autonomy_flutter/view/artwork_common_widget.dart';
 import 'package:autonomy_flutter/view/ff_artwork_thumbnail_view.dart';
 import 'package:autonomy_flutter/view/primary_button.dart';
+import 'package:autonomy_flutter/widgets/app_bar.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
